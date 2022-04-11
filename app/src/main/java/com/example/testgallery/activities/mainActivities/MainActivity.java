6d7c9da78e0dd55e5999db79000d98f4a9e5e42a
dojuun2 <99643097+dojuun2@users.noreply.github.com>
2022-04-11 23:07:11 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/testgallery/activities/mainActivities/MainActivity.java
+++ app/src/main/java/com/example/testgallery/activities/mainActivities/MainActivity.java
@@ -36,6 +36,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
 
